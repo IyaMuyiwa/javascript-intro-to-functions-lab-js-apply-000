9bb000aae1cf512886fb10b48ae2824bc4a87504
Yetunde Okediji <yetunde.okediji@gmail.com>
2018-06-19 02:48:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ var uppercase = "HELLO!"
 var lowercase = "hello!"
 
 function sayHiToGrandma(string){
+<<<<<<< HEAD
   //var uppercase = "HELLO!";
   //var lowercase = "hello!";
   //var string = "I can't hear you";
@@ -34,4 +35,17 @@ function sayHiToGrandma(string){
   if (string === "I love you, Grandma.") {
     return ("I love you, too.")
   }}
+=======
+  var uppercase = "HELLO!";
+  var lowercase = "hello!";
+  //var string = "I can't hear you";
+}
+  if(string === lowercase {
+    console.log("I can't hear you!"); 
+  }  
+  elseif (string === uppercase {
+    console.log ("YES INDEED!");
+  }
+  else (string)
+>>>>>>> 5f2c80f12b6a66a216cf1ae6edba54a77a4cdd58
  
